d6c6677aee921606d6eb2f01c66067c865e5ab07
Luke Bacon <luke@equivalentideas.com>
2016-03-11 10:47:46 +1100

    Make sure that Area properties are accessible (add test only)

--- test/everypolitician/popolo_test.rb
+++ test/everypolitician/popolo_test.rb
@@ -197,4 +197,15 @@ class Everypolitician::PopoloTest < Minitest::Test
     assert_instance_of Everypolitician::Popolo::Areas, popolo.areas
     assert_instance_of Everypolitician::Popolo::Area, area
   end
+
+  def test_accessing_area_properties
+    popolo = Everypolitician::Popolo::JSON.new(
+      areas: [{ id: '123', name: 'Newtown', type: 'constituency' }]
+    )
+    area = popolo.areas.first
+
+    assert_equal '123', area.id
+    assert_equal 'Newtown', area.name
+    assert_equal 'constituency', area.type
+  end
 end
