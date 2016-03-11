27d1d55d25aaea3bd9aaae117762cfb96491e4d2
Henare Degan <henare.degan@gmail.com>
2016-03-11 16:45:45 +1100

    Extract Organization into it's own file

--- lib/everypolitician/popolo.rb
+++ lib/everypolitician/popolo.rb
@@ -1,6 +1,7 @@
 require 'everypolitician/popolo/version'
 require 'everypolitician/popolo/collection'
 require 'everypolitician/popolo/people'
+require 'everypolitician/popolo/organization'
 require 'json'
 
 module Everypolitician
@@ -42,28 +43,6 @@ module Everypolitician
       def memberships
         Memberships.new(popolo[:memberships])
       end
-    end
-
-    class Organizations < Collection
-      def initialize(documents)
-        @documents = documents.map { |p| Organization.new(p) }
-      end
-    end
-
-    class Organization
-      attr_reader :document
-
-      def initialize(document)
-        @document = document
-        document.each do |key, value|
-          define_singleton_method(key) { value }
-        end
-      end
-
-      def ==(other)
-        id == other.id
-      end
-      alias eql? ==
     end
 
     class Areas < Collection
---
+++ lib/everypolitician/popolo/organization.rb
@@ -0,0 +1,25 @@
+module Everypolitician
+  module Popolo
+    class Organizations < Collection
+      def initialize(documents)
+        @documents = documents.map { |p| Organization.new(p) }
+      end
+    end
+
+    class Organization
+      attr_reader :document
+
+      def initialize(document)
+        @document = document
+        document.each do |key, value|
+          define_singleton_method(key) { value }
+        end
+      end
+
+      def ==(other)
+        id == other.id
+      end
+      alias eql? ==
+    end
+  end
+end
