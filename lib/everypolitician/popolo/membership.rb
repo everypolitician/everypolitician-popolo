d7f4970fc6beb59304208a164e8b3208fdf45721
Tony Bowden <tony@tmtm.com>
2016-03-21 19:26:43 +0000

    Make Membership use the new Entity class

--- lib/everypolitician/popolo/membership.rb
+++ lib/everypolitician/popolo/membership.rb
@@ -2,21 +2,8 @@ module Everypolitician
   module Popolo
     class Memberships < Collection; end
 
-    class Membership
-      def initialize(document)
-        @document = document
-        document.each do |key, value|
-          define_singleton_method(key) { value }
-        end
-      end
-
-      def start_date
-        @document[:start_date]
-      end
-
-      def end_date
-        @document[:end_date]
-      end
+    class Membership < Entity
+      self.attributes = %i(id person_id on_behalf_of_id organization_id area_id role start_date end_date).to_set
     end
   end
 end
