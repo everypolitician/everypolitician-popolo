d4ac0b353544497e69b618638d6e9ba7c421309e
Tony Bowden <tony@tmtm.com>
2016-03-21 19:27:02 +0000

    Make remaining classes inherit from Entity

--- lib/everypolitician/popolo/event.rb
+++ lib/everypolitician/popolo/event.rb
@@ -1,14 +1,6 @@
 module Everypolitician
   module Popolo
     class Events < Collection; end
-
-    class Event
-      def initialize(document)
-        @document = document
-        document.each do |key, value|
-          define_singleton_method(key) { value }
-        end
-      end
-    end
+    class Event < Entity; end
   end
 end
