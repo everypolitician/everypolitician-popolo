79ece568aa54a7d83734ac2337689d69fa68c7b6
Henare Degan <henare.degan@gmail.com>
2016-03-22 20:12:38 +1100

    Document some Collection methods

--- lib/everypolitician/popolo/collection.rb
+++ lib/everypolitician/popolo/collection.rb
@@ -3,6 +3,7 @@ module Everypolitician
     class Collection
       include Enumerable
 
+      # @return [Array] the items in this collection
       attr_reader :documents
 
       def initialize(documents)
@@ -13,15 +14,27 @@ module Everypolitician
         documents.each(&block)
       end
 
+      # Subtract items in another Collection from this based on the same *id* attribute
+      #
+      # @param other [Collection] items to subtract
+      # @return Collection with `other` removed
       def -(other)
         other_ids = Set.new(other.documents.map(&:id))
         documents.reject { |d| other_ids.include?(d.id) }
       end
 
+      # Select the first item with attributes matching the specified criteria.
+      #
+      # @param attributes [Hash] of attributes expected item must have
+      # @return The first item that matches the specified attrubutes in this collection
       def find_by(attributes = {})
         where(attributes).first
       end
 
+      # Select all items with attributes matching the specified criteria.
+      #
+      # @param attributes [Hash] of attributes
+      # @return A subset of this collection with all items that match the specified criteria
       def where(attributes = {})
         find_all do |object|
           attributes.all? { |k, v| object.send(k) == v }
