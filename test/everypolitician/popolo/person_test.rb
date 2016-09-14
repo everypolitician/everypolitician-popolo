7837c2e46b4b9adfab9c7b409f15ac98edf154af
Oliver Denman <oliver@therewillbeanimals.com>
2016-09-14 17:05:12 +0100

    Test Person against Turkey data

--- test/everypolitician/popolo/person_test.rb
+++ test/everypolitician/popolo/person_test.rb
@@ -1,17 +1,17 @@
 require 'test_helper'
 
 class PersonTest < Minitest::Test
-  def popolo
-    Everypolitician::Popolo::JSON.new(persons: [{ id: '123', name: 'Bob' }])
+  def people
+    Everypolitician::Popolo.read('test/fixtures/turkey-ep-popolo-v1.0.json').persons
   end
 
-  def bob
-    popolo.persons.first
+  def abdullah
+    @abdullah ||= people.select { |p| p.id == '51704345-f3a1-4eae-912a-85a06bd14622' }.first
   end
 
   def test_reading_popolo_people
-    assert_instance_of Everypolitician::Popolo::People, popolo.persons
-    assert_instance_of Everypolitician::Popolo::Person, bob
+    assert_instance_of Everypolitician::Popolo::People, people
+    assert_instance_of Everypolitician::Popolo::Person, abdullah
   end
 
   def test_no_persons_in_popolo_data
@@ -20,198 +20,101 @@ class PersonTest < Minitest::Test
   end
 
   def test_accessing_person_properties
-    assert bob.key?(:id)
-    assert_equal '123', bob[:id]
-  end
-
-  def test_person_twitter_contact_details
-    person = Everypolitician::Popolo::Person.new(
-      contact_details: [{ type: 'twitter', value: 'bob' }]
-    )
-    assert_equal 'bob', person.twitter
-  end
-
-  def test_person_twitter_links
-    person = Everypolitician::Popolo::Person.new(
-      links: [{ note: 'twitter', url: 'https://twitter.com/bob' }]
-    )
-    assert_equal 'https://twitter.com/bob', person.twitter
-  end
-
-  def test_person_contact_details_and_twitter_links
-    person = Everypolitician::Popolo::Person.new(
-      contact_details: [{ note: 'cell', value: '+1-555-555-0100' }],
-      links:           [{ note: 'twitter', url: 'https://twitter.com/bob' }]
-    )
-    assert_equal 'https://twitter.com/bob', person.twitter
-  end
-
-  def test_accessing_basic_person_attributes
-    person = Everypolitician::Popolo::Person.new(id: '123', name: 'Bob', other_names: [])
-    assert_equal '123', person.id
-    assert_equal 'Bob', person.name
-    assert_equal [], person.other_names
-  end
-
-  def test_person_name_at
-    assert_equal bob.name_at('2016-01-11'), 'Bob'
-    person = Everypolitician::Popolo::Person.new(
-      name:        'Bob',
-      other_names: [
-        { name: 'Robert', start_date: '1989-01-01', end_date: '1999-12-31' },
-      ]
-    )
-    assert_equal 'Robert', person.name_at('1990-06-01')
-
-    person = Everypolitician::Popolo::Person.new(
-      name:        'Bob',
-      other_names: [
-        { name: 'Robert', start_date: '1989-01-01', end_date: '1999-12-31' },
-        { name: 'Bobby', start_date: '1989-01-01', end_date: '2012-12-31' },
-      ]
-    )
-
-    assert_raises Everypolitician::Popolo::Person::Error do
-      person.name_at('1996-01-01')
-    end
-  end
-
-  def test_person_facebook
-    assert_nil bob.facebook
-    person = Everypolitician::Popolo::Person.new(
-      links: [{ note: 'facebook', url: 'https://www.facebook.com/bob' }]
-    )
-    assert_equal 'https://www.facebook.com/bob', person.facebook
-  end
-
-  def test_person_identifier
-    person = Everypolitician::Popolo::Person.new(
-      identifiers: [
-        { scheme: 'foo', identifier: 'bar' },
-        { scheme: 'wikidata', identifier: 'zap' },
-      ]
-    )
-
-    assert_equal 'bar', person.identifier('foo')
-    assert_equal 'zap', person.identifier('wikidata')
-  end
-
-  def test_person_wikidata
-    person = Everypolitician::Popolo::Person.new(
-      identifiers: [{ scheme: 'wikidata', identifier: 'Q153149' }]
-    )
-    assert_equal 'Q153149', person.wikidata
-  end
-
-  def test_person_no_wikidata
-    assert_nil bob.wikidata
-  end
-
-  def test_person_contacts
-    person = Everypolitician::Popolo::Person.new(
-      contact_details: [
-        { type: 'phone', value: '9304832' },
-        { type: 'fax',   value: '9304833' },
-      ]
-    )
-    assert_equal '9304832', person.contact('phone')
-    assert_equal '9304832', person.phone
-    assert_equal '9304833', person.fax
-  end
-
-  def test_person_no_contacts
-    assert_equal nil, bob.contact('phone')
-    assert_equal nil, bob.phone
-    assert_equal nil, bob.fax
-  end
-
-  def test_person_sort_name
-    assert_equal 'Bob', bob.sort_name
-    person = Everypolitician::Popolo::Person.new(name: 'Bob', sort_name: 'Robert')
-    assert_equal 'Robert', person.sort_name
+    assert abdullah.key?(:id)
+    assert_equal '51704345-f3a1-4eae-912a-85a06bd14622', abdullah.id
   end
 
   def test_person_family_name
-    person = Everypolitician::Popolo::Person.new(name: 'Bob', family_name: 'Roberts')
-    assert_equal 'Roberts', person.family_name
+    assert_equal 'Gül', abdullah.family_name
   end
 
   def test_person_given_name
-    person = Everypolitician::Popolo::Person.new(name: 'Bob', given_name: 'Robert')
-    assert_equal 'Robert', person.given_name
+    assert_equal 'Abdullah', abdullah.given_name
   end
 
-  def test_person_email
-    assert_equal nil, bob.email
-    person = Everypolitician::Popolo::Person.new(name: 'Bob', email: 'bob@example.org')
-    assert_equal 'bob@example.org', person.email
-  end
-
-  def test_person_image
-    assert_equal nil, bob.image
-    person = Everypolitician::Popolo::Person.new(name: 'Bob', image: 'http://example.org/img.jpeg')
-    assert_equal 'http://example.org/img.jpeg', person.image
+  def test_person_identifiers
+    abdullah_identifier = { identifier: 'abdullah_gül', scheme: 'everypolitician_legacy' }
+    assert_equal abdullah_identifier, abdullah.identifiers.first
   end
 
   def test_person_images
-    images = [
-      {
-        url: 'http://www.parliament.gov.zm/sites/default/files/styles/naz-mp-teaser/public/images/mp/Given%20Lubinda.jpg?itok=xhxCdr64',
-      },
-    ]
-    person = Everypolitician::Popolo::Person.new(name: 'Bob', images: images)
-    assert_equal images, person.images
+    abdullah_image = { url: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Abdullah_Gül_2011-06-07.jpg' }
+    assert_equal abdullah_image, abdullah.images.first
   end
 
-  def test_person_gender
-    assert_equal nil, bob.gender
-    person = Everypolitician::Popolo::Person.new(name: 'Bob', gender: 'male')
-    assert_equal 'male', person.gender
-  end
-
-  def test_person_equality_based_on_id
-    person2 = Everypolitician::Popolo::Person.new(id: '123', name: 'Bob', gender: 'male')
-    assert_equal bob, person2
-  end
-
-  def test_person_equality_based_on_class
-    organization = Everypolitician::Popolo::Organization.new(id: '123')
-    refute_equal bob, organization
-  end
-
-  def test_persons_subtraction
-    person1 = { id: '123', name: 'Alice' }
-    person2 = { id: '456', name: 'Bob', gender: 'male' }
-    all_people = Everypolitician::Popolo::People.new([person1, person2])
-    just_person_1 = Everypolitician::Popolo::People.new([person1])
-    assert_equal [Everypolitician::Popolo::Person.new(person2)], all_people - just_person_1
-  end
-
-  def test_honorific_prefix
-    assert_nil bob.honorific_prefix
-    person = Everypolitician::Popolo::Person.new(id: '123', name: 'Bob', honorific_prefix: 'Dr')
-    assert_equal 'Dr', person.honorific_prefix
-  end
-
-  def test_honorific_suffix
-    assert_nil bob.honorific_suffix
-    person = Everypolitician::Popolo::Person.new(id: '123', name: 'Bob', honorific_suffix: 'PhD')
-    assert_equal 'PhD', person.honorific_suffix
-  end
-
-  def test_person_memberships
-    popolo = Everypolitician::Popolo::JSON.new(persons: [{ id: '123', name: 'Bob' }], memberships: [{ person_id: '123', start_date: '2016-01-01' }])
-    memberships = popolo.persons.first.memberships
-    assert_equal 1, memberships.size
-    assert_equal '2016-01-01', memberships.first.start_date
+  def test_person_other_names
+    abdullah_other_names = { lang: 'af', name: 'Abdullah Gül', note: 'multilingual' }
+    assert_equal abdullah_other_names, abdullah.other_names.first
   end
 
   def test_person_sources
-    sources = { sources: [
+    assert_nil abdullah.sources
+  end
 
-      url: 'http://wolesi.website/pve/document.aspx?Page=11&Cat=37',
-    ], }
-    person = Everypolitician::Popolo::Person.new(id: '123', name: 'Bob', sources: sources)
-    assert_equal sources, person.sources
+  def test_person_email
+    assert_nil abdullah.email
+  end
+
+  def test_person_image
+    abdullah_image = 'https://upload.wikimedia.org/wikipedia/commons/9/91/Abdullah_Gül_2011-06-07.jpg'
+    assert_equal abdullah_image, abdullah.image
+  end
+
+  def test_person_gender
+    assert_equal 'male', abdullah.gender
+  end
+
+  def test_person_birth_date
+    assert_equal '1950-10-29', abdullah.birth_date
+  end
+
+  def test_person_death_date
+    assert_nil abdullah.death_date
+  end
+
+  def test_person_honorific_prefix
+    assert_nil abdullah.honorific_prefix
+  end
+
+  def test_person_honorifix_suffix
+    assert_nil abdullah.honorific_suffix
+  end
+
+  def test_person_links
+    abdullah_link = { note: 'Wikipedia (af)', url: 'https://af.wikipedia.org/wiki/Abdullah_Gül' }
+    assert_equal abdullah_link, abdullah.links.first
+  end
+
+  def test_person_contact_details
+    abdullah_contact_details = { type: 'twitter', value: 'cbabdullahgul' }
+    assert_equal abdullah_contact_details, abdullah.contact_details.first
+  end
+
+  def test_person_phone
+    assert_nil abdullah.phone
+  end
+
+  def test_person_fax
+    assert_nil abdullah.fax
+  end
+
+  def test_person_twitter
+    assert_equal 'cbabdullahgul', abdullah.twitter
+  end
+
+  def test_person_facebook
+    assert_nil abdullah.facebook
+  end
+
+  def test_person_wikidata
+    assert_equal 'Q42852', abdullah.wikidata
+  end
+
+  def test_person_sort_name
+    assert_equal 'Abdullah Gül', abdullah.sort_name
+  end
+
+  def test_person_memberships
+    assert_equal '83911419-04ab-4add-a687-93a4e8845296', abdullah.memberships.first.organization_id
   end
 end
