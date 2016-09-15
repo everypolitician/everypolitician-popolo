ef268bbf074c4ab9151276561687e1420949c384
Oliver Denman <oliver@therewillbeanimals.com>
2016-09-15 11:20:08 +0100

    Test Organization against real data
    Tests Organization against Turkey popolo.

--- test/everypolitician/popolo/organization_test.rb
+++ test/everypolitician/popolo/organization_test.rb
@@ -1,22 +1,20 @@
 require 'test_helper'
 
 class OrganizationTest < Minitest::Test
-  def test_popolo
-    Everypolitician::Popolo::JSON.new(organizations: [{ id:             '123',
-                                                        name:           'ACME',
-                                                        classification: 'party',
-                                                        identifier:     'Q288523',
-                                                        image:          'http://www.parlamentra.org/upload/iblock/09f/avidzba-f.jpg',
-                                                        links:          [{ url: 'http://www.test.com' }],
-                                                        other_names:    [{ name: 'ACME Inc' }],
-                                                        identifiers:    [{ identifier: 'Q288523', scheme: 'wikidata' }],
-                                                        seats:          42, },])
+  def organizations
+    @organizations ||= Everypolitician::Popolo.read('test/fixtures/turkey-ep-popolo-v1.0.json').organizations
   end
 
-  def test_reading_popolo_organizations
-    popolo = Everypolitician::Popolo::JSON.new(organizations: [{ id: '123', name: 'ACME' }])
-    assert_instance_of Everypolitician::Popolo::Organizations, popolo.organizations
-    organization = popolo.organizations.first
+  def organization
+    @organization ||= organizations.select { |o| o.id == 'MHP' }.first
+  end
+
+  def organization_with_seats
+    @organization_with_seats ||= organizations.select { |o| o.classification == 'legislature' }.first
+  end
+
+  def test_reading_popolo_organizationsions
+    assert_instance_of Everypolitician::Popolo::Organizations, organizations
     assert_instance_of Everypolitician::Popolo::Organization, organization
   end
 
@@ -25,16 +23,37 @@ class OrganizationTest < Minitest::Test
     assert_equal true, popolo.organizations.none?
   end
 
-  def test_accessing_organization_properties
-    organization = test_popolo.organizations.first
-    assert_equal '123', organization.id
-    assert_equal 'ACME', organization.name
+  def test_organization_id
+    assert_equal 'MHP', organization.id
+  end
+
+  def test_organization_name
+    assert_equal 'Milliyetçi Hareket Partisi', organization.name
+  end
+
+  def test_organization_classification
     assert_equal 'party', organization.classification
-    assert_equal 'http://www.parlamentra.org/upload/iblock/09f/avidzba-f.jpg', organization.image
-    assert_equal [{ url: 'http://www.test.com' }], organization.links
-    assert_equal [{ name: 'ACME Inc' }], organization.other_names
-    assert_equal [{ identifier: 'Q288523', scheme: 'wikidata' }], organization.identifiers
-    assert_equal 42, organization.seats
+  end
+
+  def test_organization_image
+    assert_equal 'https://upload.wikimedia.org/wikipedia/commons/e/e4/Milliyetçi_Hareket_Partisi_amblemi.png', organization.image
+  end
+
+  def test_organization_links
+    assert_equal [{ note: 'website', url: 'http://www.mhp.org.tr/' }], organization.links
+  end
+
+  def test_organization_other_names
+    organization_other_names = { lang: 'bar', name: 'Pårtei då Nationalistischn Bwegung', note: 'multilingual' }
+    assert_equal organization_other_names, organization.other_names.first
+  end
+
+  def test_organization_identifiers
+    assert_equal [{ identifier: 'Q251077', scheme: 'wikidata' }], organization.identifiers
+  end
+
+  def test_organization_seats
+    assert_equal 550, organization_with_seats.seats
   end
 
   def test_organization_equality_based_on_id
@@ -52,12 +71,7 @@ class OrganizationTest < Minitest::Test
   end
 
   def test_organization_wikidata
-    org = Everypolitician::Popolo::Organization.new(
-      id:          'abc',
-      name:        'ACME',
-      identifiers: [{ identifier: 'Q288523', scheme: 'wikidata' }]
-    )
-    assert_equal 'Q288523', org.wikidata
+    assert_equal 'Q348125', organizations.first.wikidata
   end
 
   def test_organization_no_wikidata
