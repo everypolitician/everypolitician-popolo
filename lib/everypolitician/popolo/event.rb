ce8dd7d314fb141c498acca62d44c44e90deb044
Oliver Denman <oliver@therewillbeanimals.com>
2016-09-20 16:10:00 +0100

    Add #people to Event
    Returns a list of Person objects

--- lib/everypolitician/popolo/event.rb
+++ lib/everypolitician/popolo/event.rb
@@ -3,6 +3,12 @@ module Everypolitician
     class Events < Collection; end
     class Event < Entity
       attr_accessor :start_date, :end_date
+
+      def people
+        ids = popolo.memberships.select { |m| m.legislative_period_id == id }
+                    .map(&:person_id)
+        popolo.persons.select { |p| ids.include? p.id }
+      end
     end
   end
 end
