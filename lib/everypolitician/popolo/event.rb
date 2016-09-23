09fc53697fbb517616d47338619a7115aee5c7d4
Oliver Denman <oliver@therewillbeanimals.com>
2016-09-23 16:54:23 +0100

    Use public search interface

--- lib/everypolitician/popolo/event.rb
+++ lib/everypolitician/popolo/event.rb
@@ -6,15 +6,13 @@ module Everypolitician
 
       def people
         raise NotImplementedError unless classification == 'legislative period'
-        ids = popolo.memberships.select { |m| m.legislative_period_id == id }
-                    .map(&:person_id)
+        ids = popolo.memberships.where(legislative_period_id: id).map(&:person_id)
         popolo.persons.select { |p| ids.include? p.id }
       end
 
       def organizations
         raise NotImplementedError unless classification == 'legislative period'
-        ids = popolo.memberships.select { |m| m.legislative_period_id == id }
-                    .map(&:on_behalf_of_id)
+        ids = popolo.memberships.where(legislative_period_id: id).map(&:on_behalf_of_id)
         popolo.organizations.select { |o| ids.include? o.id }
       end
     end
