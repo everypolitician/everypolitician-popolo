ce6eeeeb219717e20ddae7d97d9a45c061475e89
Oliver Denman <oliver@therewillbeanimals.com>
2016-09-26 13:00:21 +0100

    fixup! Create LegislativePeriod class

--- lib/everypolitician/popolo/event.rb
+++ lib/everypolitician/popolo/event.rb
@@ -1,17 +1,8 @@
 module Everypolitician
   module Popolo
     class Events < Collection; end
-    class MethodNotImplementedError < StandardError; end
     class Event < Entity
       attr_accessor :start_date, :end_date
-
-      def people
-        raise MethodNotImplementedError unless classification == 'legislative period'
-      end
-
-      def organizations
-        raise MethodNotImplementedError unless classification == 'legislative period'
-      end
     end
   end
 end
