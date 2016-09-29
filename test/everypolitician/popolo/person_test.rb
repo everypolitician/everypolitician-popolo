55f304bffa7f3203d37ee4b07c679683460ed36d
Struan Donald <struan@exo.org.uk>
2016-09-29 18:36:52 +0100

    update person tests to use real data, update data to facilitate

--- test/everypolitician/popolo/person_test.rb
+++ test/everypolitician/popolo/person_test.rb
@@ -2,16 +2,20 @@ require 'test_helper'
 
 class PersonTest < Minitest::Test
   def popolo
-    Everypolitician::Popolo::JSON.new(persons: [{ id: '123', name: 'Bob' }])
+    Everypolitician::Popolo.read('test/fixtures/estonia-ep-popolo-v1.0.json')
   end
 
-  def bob
+  def aadu
     popolo.persons.first
+  end
+
+  def aare
+    popolo.persons.to_a[1]
   end
 
   def test_reading_popolo_people
     assert_instance_of Everypolitician::Popolo::People, popolo.persons
-    assert_instance_of Everypolitician::Popolo::Person, bob
+    assert_instance_of Everypolitician::Popolo::Person, aadu
   end
 
   def test_no_persons_in_popolo_data
@@ -20,8 +24,8 @@ class PersonTest < Minitest::Test
   end
 
   def test_accessing_person_properties
-    assert bob.key?(:id)
-    assert_equal '123', bob[:id]
+    assert aadu.key?(:id)
+    assert_equal 'bc584d7f-86a1-4c2d-97b4-081971d8a1fa', aadu[:id]
   end
 
   def test_person_twitter_contact_details
@@ -54,14 +58,8 @@ class PersonTest < Minitest::Test
   end
 
   def test_person_name_at
-    assert_equal bob.name_at('2016-01-11'), 'Bob'
-    person = Everypolitician::Popolo::Person.new(
-      name:        'Bob',
-      other_names: [
-        { name: 'Robert', start_date: '1989-01-01', end_date: '1999-12-31' },
-      ]
-    )
-    assert_equal 'Robert', person.name_at('1990-06-01')
+    assert_equal aadu.name_at('2016-01-11'), 'Aadu Must'
+    assert_equal 'Are Heinvee', aare.name_at('1990-06-01')
 
     person = Everypolitician::Popolo::Person.new(
       name:        'Bob',
@@ -77,112 +75,84 @@ class PersonTest < Minitest::Test
   end
 
   def test_person_facebook
-    assert_nil bob.facebook
-    person = Everypolitician::Popolo::Person.new(
-      links: [{ note: 'facebook', url: 'https://www.facebook.com/bob' }]
-    )
-    assert_equal 'https://www.facebook.com/bob', person.facebook
+    assert_nil aare.facebook
+    assert_equal 'https://facebook.com/100000744087901', aadu.facebook
   end
 
   def test_person_identifier
-    person = Everypolitician::Popolo::Person.new(
-      identifiers: [
-        { scheme: 'foo', identifier: 'bar' },
-        { scheme: 'wikidata', identifier: 'zap' },
-      ]
-    )
-
-    assert_equal 'bar', person.identifier('foo')
-    assert_equal 'zap', person.identifier('wikidata')
+    assert_equal '233ac42e-573c-400e-8568-0ac3d4c107f9', aadu.identifier('riigikogu')
+    assert_equal 'Q12358062', aadu.identifier('wikidata')
   end
 
   def test_person_wikidata
-    person = Everypolitician::Popolo::Person.new(
-      identifiers: [{ scheme: 'wikidata', identifier: 'Q153149' }]
-    )
-    assert_equal 'Q153149', person.wikidata
+    assert_equal 'Q12358062', aadu.identifier('wikidata')
   end
 
   def test_person_no_wikidata
-    assert_nil bob.wikidata
+    assert_nil aare.wikidata
   end
 
   def test_person_contacts
-    person = Everypolitician::Popolo::Person.new(
-      contact_details: [
-        { type: 'phone', value: '9304832' },
-        { type: 'fax',   value: '9304833' },
-      ]
-    )
-    assert_equal '9304832', person.contact('phone')
-    assert_equal '9304832', person.phone
-    assert_equal '9304833', person.fax
+    assert_equal '6316629', aadu.contact('phone')
+    assert_equal '6316629', aadu.phone
+    assert_equal 'Aadu.Must@riigikogu.ee', aadu.email
   end
 
   def test_person_no_contacts
-    assert_equal nil, bob.contact('phone')
-    assert_equal nil, bob.phone
-    assert_equal nil, bob.fax
+    assert_equal nil, aare.contact('phone')
+    assert_equal nil, aare.phone
+    assert_equal nil, aare.fax
   end
 
   def test_person_sort_name
-    assert_equal 'Bob', bob.sort_name
-    person = Everypolitician::Popolo::Person.new(name: 'Bob', sort_name: 'Robert')
-    assert_equal 'Robert', person.sort_name
+    assert_equal 'Aadu Must', aadu.sort_name
+    assert_equal 'Are', aare.sort_name
   end
 
   def test_person_email
-    assert_equal nil, bob.email
-    person = Everypolitician::Popolo::Person.new(name: 'Bob', email: 'bob@example.org')
-    assert_equal 'bob@example.org', person.email
+    assert_equal nil, aare.email
+    assert_equal 'Aadu.Must@riigikogu.ee', aadu.email
   end
 
   def test_person_image
-    assert_equal nil, bob.image
-    person = Everypolitician::Popolo::Person.new(name: 'Bob', image: 'http://example.org/img.jpeg')
-    assert_equal 'http://example.org/img.jpeg', person.image
+    assert_equal nil, aare.image
+    assert_equal 'http://www.riigikogu.ee/wpcms/wp-content/uploads/ems/temp/68d27c93-66c5-4255-9af3-457a8b5ce77c.jpg', aadu.image
   end
 
   def test_person_gender
-    assert_equal nil, bob.gender
-    person = Everypolitician::Popolo::Person.new(name: 'Bob', gender: 'male')
-    assert_equal 'male', person.gender
+    assert_equal nil, aare.gender
+    assert_equal 'male', aadu.gender
   end
 
   def test_person_equality_based_on_id
-    person2 = Everypolitician::Popolo::Person.new(id: '123', name: 'Bob', gender: 'male')
-    assert_equal bob, person2
+    person = Everypolitician::Popolo::Person.new(id: 'bc584d7f-86a1-4c2d-97b4-081971d8a1fa')
+    assert_equal aadu, person
   end
 
   def test_person_equality_based_on_class
-    organization = Everypolitician::Popolo::Organization.new(id: '123')
-    refute_equal bob, organization
+    organization = Everypolitician::Popolo::Organization.new(id: 'bc584d7f-86a1-4c2d-97b4-081971d8a1fa')
+    refute_equal aadu, organization
   end
 
   def test_persons_subtraction
-    person1 = { id: '123', name: 'Alice' }
-    person2 = { id: '456', name: 'Bob', gender: 'male' }
-    all_people = Everypolitician::Popolo::People.new([person1, person2])
-    just_person_1 = Everypolitician::Popolo::People.new([person1])
-    assert_equal [Everypolitician::Popolo::Person.new(person2)], all_people - just_person_1
+    all_people = Everypolitician::Popolo::People.new([aadu.document.to_h, aare.document.to_h])
+    just_person_1 = Everypolitician::Popolo::People.new([aadu.document.to_h])
+    assert_equal [Everypolitician::Popolo::Person.new(aare.document.to_h)], all_people - just_person_1
   end
 
   def test_honorific_prefix
-    assert_nil bob.honorific_prefix
-    person = Everypolitician::Popolo::Person.new(id: '123', name: 'Bob', honorific_prefix: 'Dr')
-    assert_equal 'Dr', person.honorific_prefix
+    assert_nil aare.honorific_prefix
+    assert_equal 'Dr', aadu.honorific_prefix
   end
 
   def test_honorific_suffix
-    assert_nil bob.honorific_suffix
-    person = Everypolitician::Popolo::Person.new(id: '123', name: 'Bob', honorific_suffix: 'PhD')
-    assert_equal 'PhD', person.honorific_suffix
+    assert_nil aare.honorific_suffix
+    assert_equal 'PhD', aadu.honorific_suffix
   end
 
   def test_person_memberships
-    popolo = Everypolitician::Popolo::JSON.new(persons: [{ id: '123', name: 'Bob' }], memberships: [{ person_id: '123', start_date: '2016-01-01' }])
-    memberships = popolo.persons.first.memberships
-    assert_equal 1, memberships.size
-    assert_equal '2016-01-01', memberships.first.start_date
+    memberships = aadu.memberships
+    assert_equal 2, memberships.size
+    assert_equal '2013-10-31', memberships.first.end_date
   end
 end
