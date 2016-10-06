bd0243427446f7e0816d5d6e488726cb58bd10d9
Struan Donald <struan@exo.org.uk>
2016-10-06 17:17:42 +0100

    add real methods for area properties

--- lib/everypolitician/popolo/area.rb
+++ lib/everypolitician/popolo/area.rb
@@ -1,6 +1,22 @@
 module Everypolitician
   module Popolo
-    class Area < Entity; end
+    class Area < Entity
+      def identifiers
+        document.fetch(:identifiers, [])
+      end
+
+      def name
+        document.fetch(:name, nil)
+      end
+
+      def other_names
+        document.fetch(:other_names, [])
+      end
+
+      def type
+        document.fetch(:type, nil)
+      end
+    end
 
     class Areas < Collection
       entity_class Area
