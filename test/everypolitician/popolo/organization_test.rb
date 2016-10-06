240aa5368c2c07731156c1cee353a2c59d713fbf
Struan Donald <struan@exo.org.uk>
2016-10-06 15:44:42 +0100

    add missing property tests for organization

--- test/everypolitician/popolo/organization_test.rb
+++ test/everypolitician/popolo/organization_test.rb
@@ -13,12 +13,28 @@ class OrganizationTest < Minitest::Test
     orgs.find_by(id: 'IRL')
   end
 
+  def ekre
+    orgs.find_by(id: 'EKRE')
+  end
+
+  def eva
+    orgs.find_by(id: 'EVA')
+  end
+
+  def riigikogu
+    orgs.find_by(id: '1ba661a9-22ad-4d0f-8a60-fe8e28f2488c')
+  end
+
   def test_organizations_type
     assert_instance_of Everypolitician::Popolo::Organizations, orgs
   end
 
   def test_organization_type
     assert_instance_of Everypolitician::Popolo::Organization, orgs.first
+  end
+
+  def test_classification
+    assert_equal 'party', irl.classification
   end
 
   def test_name
@@ -31,6 +47,27 @@ class OrganizationTest < Minitest::Test
 
   def test_website
     assert_equal 'http://www.irl.ee/', irl.links.first[:url]
+  end
+
+  def test_image
+    assert_equal 'https://upload.wikimedia.org/wikipedia/commons/0/0c/EKRE_logo.png', ekre.image
+  end
+
+  def test_links
+    assert_equal [], eva.links
+    assert_equal 0, eva.links.count
+    assert_equal 1, ekre.links.count
+  end
+
+  def test_organization_has_other_names
+    other_name = { lang: 'en', name: 'Pro Patria and Res Publica Union', note: 'multilingual' }
+    assert_equal 23, irl.other_names.count
+    assert_includes irl.other_names, other_name
+  end
+
+  def test_seats
+    assert_equal nil, irl.seats
+    assert_equal 101, riigikogu.seats
   end
 
   def test_equality
