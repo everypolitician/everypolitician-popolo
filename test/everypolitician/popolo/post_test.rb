cd38331e3adcbe7279926206ee1809344f6e6964
Struan Donald <struan@exo.org.uk>
2016-10-07 10:45:34 +0100

    update posts test to use real data
    And add 'real' data in to fixture to enable this

--- test/everypolitician/popolo/post_test.rb
+++ test/everypolitician/popolo/post_test.rb
@@ -1,14 +1,21 @@
 require 'test_helper'
 
 class PostTest < Minitest::Test
-  def test_reading_popolo_posts
-    popolo = Everypolitician::Popolo::JSON.new(
-      posts: [{ id: 'womens_representative', label: "Women's Representative" }]
-    )
-    post = popolo.posts.first
+  def fixture
+    'test/fixtures/estonia-ep-popolo-v1.0.json'
+  end
 
-    assert_instance_of Everypolitician::Popolo::Posts, popolo.posts
-    assert_instance_of Everypolitician::Popolo::Post, post
+  def posts
+    @posts ||= Everypolitician::Popolo.read(fixture).posts
+  end
+
+  def mp
+    posts.find_by(id: 'member-of-parliament')
+  end
+
+  def test_object_types
+    assert_instance_of Everypolitician::Popolo::Posts, posts
+    assert_instance_of Everypolitician::Popolo::Post, mp
   end
 
   def test_no_posts_in_popolo_data
@@ -16,14 +23,16 @@ class PostTest < Minitest::Test
     assert_equal true, popolo.posts.none?
   end
 
-  def test_accessing_post_properties
-    popolo = Everypolitician::Popolo::JSON.new(
-      posts: [{ id: 'womens_representative', label: "Women's Representative" }]
-    )
-    post = popolo.posts.first
+  def test_id
+    assert_equal 'member-of-parliament', mp.id
+  end
 
-    assert_equal 'womens_representative', post.id
-    assert_equal "Women's Representative", post.label
+  def test_label
+    assert_equal 'Member of Parliament', mp.label
+  end
+
+  def test_organization_id
+    assert_equal '1ba661a9-22ad-4d0f-8a60-fe8e28f2488c', mp.organization_id
   end
 
   def test_it_returns_nil_for_missing_label
