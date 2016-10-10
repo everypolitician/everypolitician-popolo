64b7b94009805cfe791b5dbba5927e3655ce65c0
Struan Donald <struan@exo.org.uk>
2016-10-10 15:58:47 +0100

    use Turkey data for testing areas with missing identifiers etc

--- test/everypolitician/popolo/area_test.rb
+++ test/everypolitician/popolo/area_test.rb
@@ -1,28 +1,36 @@
 require 'test_helper'
 
 class AreaTest < Minitest::Test
-  def fixture
+  def estonia_fixture
     'test/fixtures/estonia-ep-popolo-v1.0.json'
   end
 
-  def areas
-    @areas ||= Everypolitician::Popolo.read(fixture).areas
+  def turkey_fixture
+    'test/fixtures/turkey-ep-popolo-v1.0.json'
+  end
+
+  def estonia_areas
+    @estonia_areas ||= Everypolitician::Popolo.read(estonia_fixture).areas
+  end
+
+  def turkey_areas
+    @turkey_areas ||= Everypolitician::Popolo.read(turkey_fixture).areas
   end
 
   def tartu
-    areas.find_by(name: 'Tartu linn')
+    estonia_areas.find_by(name: 'Tartu linn')
   end
 
-  def ida
-    areas.find_by(name: 'Ida-Virumaa')
+  def adana
+    @adana ||= turkey_areas.find_by(id: 'area/adana')
   end
 
   def test_areas_class
-    assert_instance_of Everypolitician::Popolo::Areas, areas
+    assert_instance_of Everypolitician::Popolo::Areas, estonia_areas
   end
 
   def test_area_class
-    assert_instance_of Everypolitician::Popolo::Area, areas.first
+    assert_instance_of Everypolitician::Popolo::Area, estonia_areas.first
   end
 
   def test_id
@@ -34,25 +42,28 @@ class AreaTest < Minitest::Test
   end
 
   def test_type
-    assert_equal nil, ida.type
+    # we don't have any data that has a missing area type so generate one
+    area = Everypolitician::Popolo::Area.new(id: 'an_area', name: 'An Area')
+    assert_equal nil, area.type
     assert_equal 'constituency', tartu.type
   end
 
   def test_other_names
     other_name = { lang: 'fr', name: "Dixième circonscription législative d'Estonie", note: 'multilingual' }
-    assert_equal [], ida.other_names
+    assert_equal [], adana.other_names
     assert_equal 3, tartu.other_names.count
     assert_includes tartu.other_names, other_name
   end
 
   def test_identifiers
     identifier = { identifier: 'Q3032626', scheme: 'wikidata' }
-    assert_equal [], ida.identifiers
+    assert_equal [], adana.identifiers
     assert_equal 1, tartu.identifiers.count
     assert_includes tartu.identifiers, identifier
   end
 
   def test_wikidata
+    assert_equal nil, adana.wikidata
     assert_equal 'Q3032626', tartu.wikidata
   end
 end
